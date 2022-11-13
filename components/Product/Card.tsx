eaca2073d6b6227a0f1601d3adbe7f5555a16c64
Caleb Smith <minifiedcode@gmail.com>
2022-11-13 14:41:59 -0600

    Cleanup unused code

--- components/Product/Card.tsx
+++ components/Product/Card.tsx
@@ -1,29 +1,12 @@
 import { useEffect } from "react";
-import { Product } from "../../utils/Product";
-import Image from "next/image";
-import ProductHeaders from "./Headers";
 
 export default function ProductCard({ product, updateProducts }: any) {
   useEffect(() => {}, [product]);
-
-  const removeProduct = async () => {
-    await Product.remove(product.id);
-    await updateProducts();
-  };
 
   return (
     <>
       <div className="w-full lg:w-1/3 mx-auto border-b-2 border-gray-500">
         <div className="flex py-5">
-          {/* <div className="m-auto px-2" onClick={() => removeProduct()}>
-            <Image
-              src="/x-button.svg"
-              alt="Delete product button"
-              height={48}
-              width={48}
-            />
-          </div> */}
-
           <div className="w-1/6 mx-1 px-2">
             <div>{product.id}</div>
           </div>
